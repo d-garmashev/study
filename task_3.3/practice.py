352a93ea9ea2662940222b10d0b5735b6795edf2
Dmitry Garmashev <31664765+d-garmashev@users.noreply.github.com>
2017-10-03 03:53:38 +0300

    task_3.3

--- task_3.3/practice.py
+++ task_3.3/practice.py
@@ -1,6 +1,7 @@
 import requests
+import io
 
-def translate_it(text):
+def translate_it(source_file_path, result_file_path, from_lang, to_lang='ru'):
     """
     YANDEX translation plugin
 
@@ -20,13 +21,30 @@ def translate_it(text):
     url = 'https://translate.yandex.net/api/v1.5/tr.json/translate'
     key = 'trnsl.1.1.20161025T233221Z.47834a66fd7895d0.a95fd4bfde5c1794fa433453956bd261eae80152'
 
+    with io.open(source_file_path, 'r', encoding='utf8') as f:
+         text = f.read()
+
     params = {
         'key': key,
-        'lang': 'ru-en',
-        'text': text,
-    }
-    response = requests.get(url, params=params).json()
-    return ' '.join(response.get('text', []))
+        'lang': '-'.join([from_lang, to_lang]),
+        'text': text
+        }
 
-a = translate_it('Привет')
-print(a)
+    response = requests.get(url, params=params).json()
+    text_result = ' '.join(response.get('text', []))
+
+    with io.open(result_file_path, 'w', encoding='utf8') as f:
+        f.write(text_result)
+
+# DE -> RU translation
+translate_it('DE.txt', 'DE_RU.txt', 'de')
+
+# ES -> RU translation
+translate_it('ES.txt', 'ES_RU.txt', 'es')
+
+# FR -> RU translation
+translate_it('FR.txt', 'FR_RU.txt', 'fr')
+
+# FR -> RU translation
+translate_it('FR.txt', 'FR_EN.txt', 'fr', 'en')
+
